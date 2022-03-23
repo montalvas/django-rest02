02363cd89df9c8e28b5950fd8cd461a2ffc39b01
montalvas <lucas_montalvani@hotmail.com>
2022-03-23 14:19:31 -0300

    relacoes em rest

--- core/serializers.py
+++ core/serializers.py
@@ -24,6 +24,15 @@ class AvaliacaoSerializer(serializers.ModelSerializer):
         )
         
 class CursoSerializer(serializers.ModelSerializer):
+    # Nested Relationship - bom para relacionamento 1 para 1
+    # Para mais relações pode tornar o sistema lento
+    # avaliacoes = AvaliacaoSerializer(many=True, read_only=True)
+    
+    # HyperLinked Related Field - cria links para cada relacionamento
+    avaliacoes = serializers.HyperlinkedRelatedField(many=True, read_only=True, view_name='avaliacao-detail')
+    
+    # Primary Key Related Field
+    # avaliacoes = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
     
     class Meta:
         model = Curso
@@ -32,5 +41,6 @@ class CursoSerializer(serializers.ModelSerializer):
             'titulo',
             'url',
             'data_criacao',
-            'ativo'
+            'ativo',
+            'avaliacoes'
         )
